885d890b533d8c992eebfdfe4d7b8757781efe7c
Danie Leeuwner <dvl@waleeuwner.co.za>
2019-04-22 14:34:52 +0200

    Added filepack functionality

---
+++ bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const filepack = require("../lib/filepack");
+
+filepack.filepack();
---
+++ test/dist/output.js
@@ -0,0 +1,35 @@
+/*
+Created by filepack
+Date: Monday, April 22, 2019
+*/
+
+/*
+File: src/file1.js
+*/
+
+console.log('file 1');
+
+/*
+File: src/file2.js
+*/
+
+console.log('file 2');
+
+/*
+File: src/file3.js
+*/
+
+console.log('file 3');
+
+/*
+File: src/file4.js
+*/
+
+console.log('file 4');
+
+/*
+File: src/file5.js
+*/
+
+console.log('file 5');
+
